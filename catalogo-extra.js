4fbc968555b07fe9aef134176ca0d7bd188b486f
kuadm <kuadmff2@gmail.com>
2026-09-03 17:53:49 -0400

    Adicionar curso Administração ao catálogo

---
+++ catalogo-extra.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "9": {
+    emoji: "🏢",
+    nome: "Administração",
+    formacao: "Bacharelado",
+    duracao: "3 anos",
+    mensalidade: "R$ 112,20",
+    estagio: "Confirmar com a secretaria"
+  }
+};
